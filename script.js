37bb016795ce99ad176eef3c11bd191ddc4b8356
jantons25 <ja73133910@gmail.com>
2023-12-31 12:15:26 -0500

    Se agrega el Proyecto

---
+++ script.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector('.nav__menu');
+const menuList = document.querySelector('.nav__list');
+const links = document.querySelectorAll('.nav__link')
+
+menu.addEventListener('click',function(){
+    menuList.classList.toggle('nav__list--show')
+})
+
+links.forEach(function(link){
+    link.addEventListener('click',function(){
+        menuList.classList.remove('nav__list--show');
+    })
+})
+
+// Mueve el elemento al mismo tiempo que la imagen
+var contenedor = document.getElementById('.header__portada');
+var elementoMovible = document.getElementById('.header__figure');
+
+contenedor.addEventListener('mousemove', function(event) {
+var x = event.clientX - contenedor.getBoundingClientRect().left;
+var y = event.clientY - contenedor.getBoundingClientRect().top;
+
+elementoMovible.style.left = x + 'px';
+elementoMovible.style.top = y + 'px';
+});
